154b305cf7f95b96f7de08e00f261ca0c554ea84
Lidiane Carv <lidigugasgb@gmail.com>
2023-11-23 09:25:43 -0300

    initial commit

---
+++ SearchInsert.java
@@ -0,0 +1,16 @@
+public class SearchInsert {
+    public static void main(String[] args) {
+        int [] nums = {1,3,5,6}; int target = 8;
+        boolean found = false; int index = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            int num = nums[i];
+            if (num >= target) {
+                found = true; index = i;
+                break;
+            }
+        }
+        if (found == false) System.out.println(nums.length);
+        else System.out.println(index);
+    }
+}
