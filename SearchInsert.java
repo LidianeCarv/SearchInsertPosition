4f761c3324e6a91240e1054d5965e2850221cba9
Lidiane Carv <lidigugasgb@gmail.com>
2023-11-23 09:49:20 -0300

    initial commit II

--- SearchInsert.java
+++ SearchInsert.java
@@ -2,7 +2,7 @@ public class SearchInsert {
     public static void main(String[] args) {
         int [] nums = {1,3,5,6}; int target = 8;
         boolean found = false; int index = 0;
-
+        // initial commit
         for (int i = 0; i < nums.length; i++){
             int num = nums[i];
             if (num >= target) {
